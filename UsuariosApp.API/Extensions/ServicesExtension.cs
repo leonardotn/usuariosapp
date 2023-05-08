9717f22e5c2e4916fbe6fc00a2e79fb71f900735
leonardotn <leo.t.n@hotmail.com>
2023-05-08 13:14:21 -0300

    feat: desenvolver os applications services e dtos

---
+++ UsuariosApp.API/Extensions/ServicesExtension.cs
@@ -0,0 +1,15 @@
+﻿using UsuariosApp.Application.Interfaces;
+using UsuariosApp.Application.Services;
+
+namespace UsuariosApp.API.Extensions
+{
+    public static class ServicesExtension
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddTransient<IUsuarioAppService, UsuarioAppService>();
+
+            return services;
+        }
+    }
+}
