4a91e8c5cf44f5e7cc41f10885adc2d03506fd51
leonardotn <leo.t.n@hotmail.com>
2023-05-29 09:44:32 -0300

    Correção de projeto Infra

---
+++ UsuariosApp.Infra.Identity/Settings/IdentitySettings.cs
@@ -0,0 +1,9 @@
+﻿namespace UsuariosApp.Infra.Identity.Settings
+{
+    public class IdentitySettings
+    {
+        public string? Issuer { get; set; }
+        public string? Audience { get; set; }
+        public string? SecretKey { get; set; }
+    }
+}
